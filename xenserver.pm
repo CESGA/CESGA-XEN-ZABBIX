fb5e3683f74363cda149931648721590ae8b5340
Galicia Supercomputing Center (CESGA) <natalia@cesga.es>
2012-01-27 10:26:25 +0100

    Replaced all mysql queries with api calls

--- xenserver.pm
+++ xenserver.pm
@@ -20,10 +20,7 @@
 
 #data structure for storing values of a xenserver instance
 use Zabbix;
-
 package xenserver;
-use DBI;
-use Data::Dumper;
 
 sub new
 {
@@ -80,78 +77,53 @@ sub submit_zabbix {
 
 	#send DOMUs info through perl zabbix api
 	#1.- check if exist in this xen server
-	#change mysql query to api query
-	$dbh = DBI->connect('dbi:mysql:zabbix','user_mysql_zabbix','pass_mysql_zabbix') or die "Connection Error: $DBI::errstr\n";
-	$query_select="select itemid,key_,hostid from items where key_ like 'domu_".$dom0.$domain."%'";
-	$sth_items = $dbh->prepare($query_select);
-	$sth_items->execute or die "SQL Error: $DBI::errstr\n";
+	my $zabbixsession = Net::Zabbix->new($main::ZABBIX_URL,$main::ZABBIX_USER_API,$main::ZABBIX_USER_PASS);
+	#this should return 3 items: domu_$dom0.$domain,domu_$dom0.$domain_mem and domu_$dom0.$domain_cpu
+	my $itemids = $zabbixsession->get('item', { output => 'extend', patternKey => "domu_".$dom0.$domain });
+	my @my_array=$itemids->{'result'};
 
-        #if this return 0 items.. we must create items for this domus
-	
- 	if ($sth_items->rows < 1 ){
+	#if this return 0 items.. we must create items for this domus
+	if(not defined $my_array[0][0]->{'itemid'}){
 		#domu name
-                my $zabbixsession = Net::Zabbix->new("http://url.zabbix.fronted","user_api","pass_api");
+                my $zabbixsession = Net::Zabbix->new($main::ZABBIX_URL,$main::ZABBIX_USER_API,$main::ZABBIX_USER_PASS);
                 my $itemid_created = $zabbixsession->post('item', { description => 'domu_'.$dom0.$domain, key_ => 'domu_'.$dom0.$domain, hostid => $self->{_xen_hostid}, data_type => 0, type => 2,value_type => 4,trapper_hosts => ''});
 		#domu memory
                 my $itemid_created1 = $zabbixsession->post('item', { description => 'domu_'.$dom0.$domain.'_mem', key_ => 'domu_'.$dom0.$domain.'_mem', hostid => $self->{_xen_hostid}, units => 'B',formula => 1048576,data_type => 0, type => 2,value_type => 3,trapper_hosts => '',multiplier => '1'});
 		#domu cpu
 		my $itemid_created2 = $zabbixsession->post('item', { description => 'domu_'.$dom0.$domain.'_cpu', key_ => 'domu_'.$dom0.$domain.'_cpu', hostid => $self->{_xen_hostid}, data_type => 0, type => 2,value_type => 0,delta => 1, units => '%', trapper_hosts => ''});
 
-
-		#do you need the itemid in zabbix, uncomment this
-		#@my_array=%{$itemid_created->{'result'}};
-		#$itemid=$my_array[1][0];
-
-		#DON'T KNOW THE REASON, BUT IF I CREATE THE ITEMS, I CAN'T UPDATE THEM A MOMENT AFTER.. SO IN NEXT EXECUTION, THEY WILL BE UPDATED
-		#IF SOMEONE, KNOWS THE REASON AND HOW TO FIX IT, JUST UNCOMMENT THESE LINES TO GET VALUES UPDATED
-		#update domu_name
-		#$command="/usr/local/bin/zabbix_sender --zabbix-server ip-zabbix-server --host ".$self->{_xen_host}." --key domu_".$dom0.$domain." --value \"".$domain."\"";
-		# update domu_name_mem
-		#$command1="/usr/local/bin/zabbix_sender --zabbix-server ip-zabbix-server --host ".$self->{_xen_host}." --key domu_".$dom0.$domain."_mem --value \"".$mem."\"";
-                # update domu_name_cpu
-                #$command2="/usr/local/bin/zabbix_sender --zabbix-server ip-zabbix-server --host ".$self->{_xen_host}." --key domu_".$dom0.$domain."_cpu --value \"".$cpu."\"";
-		#$out=`$command`;
-		#$out=`$command1`;
-		#$out=`$command2`;
 		}
 	#otherwise we must check if items belongs to this host, it they belong, we submit the data, otherwise we reassign the item to this xen-server (maybe it was running in other xen server previous 
 	else {
 		#process first row to check the hostid
-		@row_item = $sth_items->fetchrow_array;
+		if ($my_array[0][0]->{'hostid'} == $self->{_xen_hostid}){
 		#if they match, we are using the correct host, so, just send info to items
-		if ($row_item[2] == $self->{_xen_hostid}){
                         #update domu_name
-	                $command="/usr/local/bin/zabbix_sender --zabbix-server ip-zabbix-server --host ".$self->{_xen_host}." --key domu_".$dom0.$domain." --value \"".$domain."\" -vv";
+	                $command="$main::PATH_ZABBIX_SENDER_BIN --zabbix-server $main::ZABBIX_SERVER_IP --host ".$self->{_xen_host}." --key domu_".$dom0.$domain." --value \"".$domain."\" -vv";
        		        # update domu_name_mem
-	                $command1="/usr/local/bin/zabbix_sender --zabbix-server ip-zabbix-server --host ".$self->{_xen_host}." --key domu_".$dom0.$domain."_mem --value \"".$mem."\" -vv";
+	                $command1="$main::PATH_ZABBIX_SENDER_BIN --zabbix-server $main::ZABBIX_SERVER_IP --host ".$self->{_xen_host}." --key domu_".$dom0.$domain."_mem --value \"".$mem."\" -vv";
 			# update domu_name_cpu
-			$command2="/usr/local/bin/zabbix_sender --zabbix-server ip-zabbix-server --host ".$self->{_xen_host}." --key domu_".$dom0.$domain."_cpu --value \"".$cpu."\" -vv";
+			$command2="$main::PATH_ZABBIX_SENDER_BIN --zabbix-server $main::ZABBIX_SERVER_IP --host ".$self->{_xen_host}." --key domu_".$dom0.$domain."_cpu --value \"".$cpu."\" -vv";
 
 			#execute commands
 			$out=`$command`;
 			$out=`$command1`;	
 			$out=`$command2`;
 			}
+		#if host doesnt match current host, update items
 		else{
-			#reassign domu to new host
-			$query_update="update items set hostid=".$self->{_xen_hostid}." where key_ = 'domu_".$domain."'";
-                        $query_update1="update items set hostid=".$self->{_xen_hostid}." where key_ = 'domu_".$domain."_mem'";
-                        $query_update2="update items set hostid=".$self->{_xen_hostid}." where key_ = 'domu_".$domain."_cpu'";
-
-			$update = $dbh->prepare($query_update);
-		        $update->execute or die "SQL Error: $DBI::errstr\n";
-			$update = $dbh->prepare($query_update1);
-                        $update->execute or die "SQL Error: $DBI::errstr\n";
-			$update = $dbh->prepare($query_update2);
-                        $update->execute or die "SQL Error: $DBI::errstr\n";
+			#reassign domus items to this xen server 
+			my $itemid_updated = $zabbixsession->update('item', { itemid => $my_array[0][0]->{'itemid'},hostid => $self->{_xen_hostid} });
+                        my $itemid_updated1 = $zabbixsession->update('item', { itemid => $my_array[0][1]->{'itemid'},hostid => $self->{_xen_hostid} });
+                        my $itemid_updated2 = $zabbixsession->update('item', { itemid => $my_array[0][2]->{'itemid'},hostid => $self->{_xen_hostid} });
 
 			#send info 
-                        #update domu_name
-                        $command="/usr/local/bin/zabbix_sender --zabbix-server ip-zabbix-server --host ".$self->{_xen_host}." --key domu_".$dom0.$domain." --value \"".$domain."\"";
-                        # update domu_name_mem
-                        $command1="/usr/local/bin/zabbix_sender --zabbix-server ip-zabbix-server --host ".$self->{_xen_host}." --key domu_".$dom0.$domain."_mem --value \"".$mem."\"";
-                        # update domu_name_cpu
-                        $command2="/usr/local/bin/zabbix_sender --zabbix-server ip-zabbix-server --host ".$self->{_xen_host}." --key domu_".$dom0.$domain."_cpu --value \"".$cpu."\"";
+                        # send domu_name
+                        $command="$main::PATH_ZABBIX_SENDER_BIN --zabbix-server $main::ZABBIX_SERVER_IP --host ".$self->{_xen_host}." --key domu_".$dom0.$domain." --value \"".$domain."\"";
+                        # send domu_name_mem
+                        $command1="$main::PATH_ZABBIX_SENDER_BIN --zabbix-server $main::ZABBIX_SERVER_IP --host ".$self->{_xen_host}." --key domu_".$dom0.$domain."_mem --value \"".$mem."\"";
+                        # send domu_name_cpu
+                        $command2="$main::PATH_ZABBIX_SENDER_BIN --zabbix-server $main::ZABBIX_SERVER_IP --host ".$self->{_xen_host}." --key domu_".$dom0.$domain."_cpu --value \"".$cpu."\"";
 
                         #execute commands
                         $out=`$command`;
@@ -166,12 +138,12 @@ sub submit_zabbix {
 
     
     #execute only if xen_status = OK 
-    $out=`/usr/local/bin/zabbix_sender --zabbix-server ip-zabbix-server --port 10051 --host $self->{_xen_host} --key xen_status --value "$self->{_xen_status}"`;
-    $out=`/usr/local/bin/zabbix_sender --zabbix-server ip-zabbix-server --port 10051 --host $self->{_xen_host} --key xen_mem --value "$self->{_xen_mem}"`;
+    $out=`$main::PATH_ZABBIX_SENDER_BIN --zabbix-server $main::ZABBIX_SERVER_IP --host $self->{_xen_host} --key xen_status --value "$self->{_xen_status}"`;
+    $out=`$main::PATH_ZABBIX_SENDER_BIN --zabbix-server $main::ZABBIX_SERVER_IP --host $self->{_xen_host} --key xen_mem --value "$self->{_xen_mem}"`;
 
     #mem_free = total - used by domus
     $free=$self->{_xen_mem}-$total_used;
-    $out=`/usr/local/bin/zabbix_sender --zabbix-server ip-zabbix-server --port 10051 --host $self->{_xen_host} --key xen_mem_free --value "$free"`;
+    $out=`$main::PATH_ZABBIX_SENDER_BIN --zabbix-server $main::ZABBIX_SERVER_IP --host $self->{_xen_host} --key xen_mem_free --value "$free"`;
 
 	
 
